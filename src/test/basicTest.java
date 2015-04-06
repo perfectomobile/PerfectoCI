d9e4a03eaac147bc6595732c0e23e7a316712500
uzie <PerfectoMobile256@gmail.com>
2015-04-06 10:52:08 -0400

    add before/after/exe

--- src/test/basicTest.java
+++ src/test/basicTest.java
@@ -1,15 +1,19 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.DriverCommand;
+import org.openqa.selenium.remote.RemoteExecuteMethod;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class basicTest {
+public abstract class basicTest {
 
 	static RemoteWebDriver _driver;
 	public basicTest() {
@@ -34,5 +38,26 @@ public class basicTest {
 		}		return objects.iterator();
 	}
 	
-	 
+	public abstract void beforeTest() throws Exception ;
+	public abstract void execTest() throws Exception ;
+	public abstract void endTest()  ;
+	
+	public void switchToContext(RemoteWebDriver driver, String context) {
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("name", context);
+		executeMethod.execute(DriverCommand.SWITCH_TO_CONTEXT, params);
+	}
+
+	public String getCurrentContextHandle(RemoteWebDriver driver) {		  
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		String context =  (String) executeMethod.execute(DriverCommand.GET_CURRENT_CONTEXT_HANDLE, null);
+		return context;
+	}
+
+	public List<String> getContextHandles(RemoteWebDriver driver) {		  
+		RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+		List<String> contexts =  (List<String>) executeMethod.execute(DriverCommand.GET_CONTEXT_HANDLES, null);
+		return contexts;
+	}
 }
